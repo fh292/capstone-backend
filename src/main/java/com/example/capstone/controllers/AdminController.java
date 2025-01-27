2c94fdcce3bfa9914cb5fdf36b4ba17b428cbaca
f h <falhamad2002@gmail.com>
2025-01-27 11:22:46 +0300

    added admin controller

---
+++ src/main/java/com/example/capstone/controllers/AdminController.java
@@ -0,0 +1,9 @@
+package com.example.capstone.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/admin")
+@RestController
+public class AdminController {
+}
