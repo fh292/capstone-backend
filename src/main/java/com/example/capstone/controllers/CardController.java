6ffea0f97cbe4312ebc3bdea8e5cee20c4717cdc
f h <falhamad2002@gmail.com>
2025-02-02 13:12:08 +0300

    minor changes

--- src/main/java/com/example/capstone/controllers/CardController.java
+++ src/main/java/com/example/capstone/controllers/CardController.java
@@ -1,9 +1,17 @@
 package com.example.capstone.controllers;
 
+import com.example.capstone.services.CardService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/card")
 @RestController
 public class CardController {
+
+    private final CardService cardService;
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+
 }
